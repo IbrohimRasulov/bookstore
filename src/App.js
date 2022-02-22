5f78091f3309760de58a72f778beb62add66ed55
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-23 01:45:04 +0500

    Create components files and add Route to App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//? Components
+import Navbar from './components/Navbar';
+import BooksContainer from './components/BooksContainer';
+import CategoriesContainer from './components/CategoriesContainer';
+import Form from './components/Form';
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/books" element={<BooksContainer />} />
+          <Route path="/categories" element={<CategoriesContainer />} />
+        </Routes>
+      </div>
+      <Form />
+    </div>
+  </Router>
+);
+
+export default App;
