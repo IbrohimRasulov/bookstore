49186e725d4daacdb56d1f2a1edf8680b636a781
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-25 23:53:04 +0500

    Remove unused codes

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,17 +1,7 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [
-  // { title: 'If qal\'asi maxbusi', category: 'fiction', id: 0 },
-  // { title: 'Qalb ko\'zgusidagi illatlar', category: 'religious', id: 1 },
-  // { title: 'Stive Jobs', category: 'development', id: 2 },
-  // { title: 'If qal\'asi maxbusi', category: 'fiction', id: 0 },
-  // { title: 'Qalb ko\'zgusidagi illatlar', category: 'religious', id: 1 },
-  // { title: 'Stive Jobs', category: 'development', id: 2 },
-  // { title: 'If qal\'asi maxbusi', category: 'fiction', id: 0 },
-  // { title: 'Qalb ko\'zgusidagi illatlar', category: 'religious', id: 1 },
-  // { title: 'Stive Jobs', category: 'development', id: 2 },
-];
+const initialState = [];
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
