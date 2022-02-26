95d47c3d3f1c1113217e7b2ea6cb182074a51189
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-27 03:14:11 +0500

    Update UI

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_FROM_API = 'bookStore/books/GET_FROM_API';
 const API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ENEJBCELxc1apDnqf6rp/books';
 
 const initialState = [];
@@ -13,6 +14,11 @@ export const addBook = (payload) => ({
 
 export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
+  payload,
+});
+
+export const getBooks = (payload) => ({
+  type: GET_FROM_API,
   payload,
 });
 
@@ -44,12 +50,31 @@ export const removeBookFromAPI = (id) => async (dispatch) => {
   await axios.delete(`${API}/${id}`);
 };
 
+export const getBooksFromAPI = () => async (dispatch) => {
+  const response = await axios.get(API);
+
+  const books = Object.entries(response.data).map(([id, arr]) => {
+    const { title, category } = arr[0];
+    const { realTitle, realAuthor } = JSON.parse(title);
+    return {
+      id,
+      title: realTitle,
+      author: realAuthor,
+      category,
+    };
+  });
+
+  dispatch(getBooks(books));
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
+    case GET_FROM_API:
+      return action.payload;
     default:
       return state;
   }
